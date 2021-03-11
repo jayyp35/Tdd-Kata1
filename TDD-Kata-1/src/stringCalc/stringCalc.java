3fa810c30aeaf133bac0fd1a9260268c09d514d2
jayyp35 <jprabhu35@gmail.com>
2021-03-12 01:04:55 +0530

    Return sum of 2 numbers

--- TDD-Kata-1/src/stringCalc/stringCalc.java
+++ TDD-Kata-1/src/stringCalc/stringCalc.java
@@ -6,6 +6,11 @@ public class stringCalc {
 	public static int Add(String numbers) {
 		if(numbers.isEmpty())
 			return 0;
-		else return -1;
+		else {
+			String[] nums = numbers.split(",");
+			
+			int sum = Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+			return sum;
+		}
 	}
 }
