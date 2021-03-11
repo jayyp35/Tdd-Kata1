186a913ff1bf9a7028e21be327e4468bd9a9cff3
jayyp35 <jprabhu35@gmail.com>
2021-03-12 01:20:32 +0530

    Support Different Delimiters

--- TDD-Kata-1/src/stringCalc/stringCalc.java
+++ TDD-Kata-1/src/stringCalc/stringCalc.java
@@ -6,7 +6,23 @@ public class stringCalc {
 	public static int Add(String numbers) {
 		if(numbers.isEmpty())
 			return 0;
-		else {
+		else if (numbers.startsWith("//")) {
+			String delimiter = "\n";
+			delimiter = numbers.charAt(2) + "|" + delimiter;
+			
+			numbers = numbers.substring(4);
+			String[] nums = numbers.split(delimiter);
+			
+			int sum = 0;
+			
+			for(int i=0;i<nums.length;i++) {
+				sum += Integer.parseInt(nums[i]);
+			}
+			
+			return sum;
+			
+			
+		} else {
 			String[] nums = numbers.split(",|\n");
 			
 			int sum = 0;
