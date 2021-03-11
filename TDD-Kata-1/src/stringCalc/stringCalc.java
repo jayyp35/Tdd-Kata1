9d96b152acfa4cb53e97c53adb603595ab83aca4
jayyp35 <jprabhu35@gmail.com>
2021-03-12 01:00:06 +0530

    Return 0 if null string is passed

--- TDD-Kata-1/src/stringCalc/stringCalc.java
+++ TDD-Kata-1/src/stringCalc/stringCalc.java
@@ -1,5 +1,11 @@
 package stringCalc;
+import java.util.*;
 
 public class stringCalc {
-
+	
+	public static int Add(String numbers) {
+		if(numbers.isEmpty())
+			return 0;
+		else return -1;
+	}
 }
