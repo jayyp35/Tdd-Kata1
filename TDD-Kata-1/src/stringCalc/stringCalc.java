302b30a9e28100e08577e0f33675f4c94bf67c7c
jayyp35 <jprabhu35@gmail.com>
2021-03-12 02:34:48 +0530

    Added multiple delimiter support

--- TDD-Kata-1/src/stringCalc/stringCalc.java
+++ TDD-Kata-1/src/stringCalc/stringCalc.java
@@ -14,8 +14,37 @@ public class stringCalc {
 			String delimiter = "\n";
 			
 			if(index > 3) {
-				String delim = numbers.substring(3,index-1);
-				delimiter = delim + "|" + delimiter;
+//				String delim = numbers.substring(3,index-1);
+//				delimiter = delim + "|" + delimiter;
+//				numbers = numbers.substring(index+1);
+				ArrayList<String> delimiters = new ArrayList<>();
+				String temp = "";
+				for(int i=2;i<index;i++) {
+					if(numbers.charAt(i)=='[' || numbers.charAt(i)==']')
+						continue;
+					else {
+						if(i==3) {
+							temp += numbers.charAt(i);
+							continue;
+						}
+						
+						if(numbers.charAt(i)==temp.charAt(0))
+							temp+= numbers.charAt(i);
+						else {
+							delimiters.add(temp);
+							temp = "" + numbers.charAt(i);
+						}
+					}
+				}
+				delimiters.add(temp);
+				
+				String delim = "";
+				for( int j=0;j<delimiters.size();j++) {
+					delim += "|";
+					delim += delimiters.get(j);
+				}
+				
+				delimiter = delimiter + delim;
 				numbers = numbers.substring(index+1);
 			} else {
 				delimiter = numbers.charAt(2) + "|" + delimiter;
